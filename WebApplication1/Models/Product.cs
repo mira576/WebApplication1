0f7936362c2acfb5912f8ebfef36cbbcebca1b43
mira576 <marmoura576@hotmail.com>
2018-07-23 00:33:36 +0300

    added context and product and category

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string NameEng { get; set; }
+        public string NameAr { get; set; }
+        public long price { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
